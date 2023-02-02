49aca1131c6d1d3bba904f71ea649289b1498a76
Anastasia <nastena.maiorova@gmail.com>
2023-02-02 21:04:49 +0300

    Подключили index.html

--- shop/home_page/views.py
+++ shop/home_page/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+#from .models import Employee
+#from .models import Guide
+#from django.http import HttpResponse
+
+#import json
+
+def index(request):
+    return render(
+    request,  # Запрос
+    'home_page/index.html',  # путь к шаблону
+    #context  # подстановки словарь
+    )
+
 
 # Create your views here.
