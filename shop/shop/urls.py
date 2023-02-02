a5de65eb428cdfbf394db444c0e4e1030dbb426e
Anastasia <nastena.maiorova@gmail.com>
2023-02-02 20:34:26 +0300

    urls подключение главной страницы

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    #path('', default_urlconf), # восстановили ракету
     path('admin/', admin.site.urls),
+    path('home_page/', include('home_page.urls')),
 ]
